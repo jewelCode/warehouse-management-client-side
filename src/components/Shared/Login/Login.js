69ef1dd005318fda59d69497660a0b4d01c5c87d
jewelCode <jewelbarua101@gmail.com>
2022-05-02 20:10:32 +0600

    login part completed

--- src/components/Shared/Login/Login.js
+++ src/components/Shared/Login/Login.js
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     let navigate = useNavigate();
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
-    if(user){
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+    if (user) {
         navigate(from, { replace: true });
     }
     return (
         <div className="w-50 mx-auto mt-5 bg-light p-5 shadow">
             <h3 className="text-center">Login:</h3>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email Address:</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Login
-                    </Button>
-                    <br />
-                    <p className="text-dark">Don't Have an Account? 
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email Address:</Form.Label>
+                    <Form.Control type="email" value={email}
+                        onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" value={password}
+                        onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" onClick={() => signInWithEmailAndPassword(email, password)}>
+                    Login
+                </Button>
+                <br />
+                <p className="text-dark">Don't Have an Account?
                     <br /><Link to="/register">Please Register</Link></p>
-                </Form>
+            </Form>
             <button onClick={() => signInWithGoogle()}>Google Sign In</button>
         </div>
     );
--- src/components/Shared/Register/Register.js
+++ src/components/Shared/Register/Register.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, } from 'react-router-dom';
+import auth from '../../../firebase.init';
+
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
     return (
         <div className="w-50 mx-auto mt-5 bg-light p-5 shadow">
             <h3 className="text-center">Register</h3>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Name:</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Your Name" />
+                    <Form.Control type="text" name="name" placeholder="Enter Your Name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email Address:</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Your Email" />
+                    <Form.Control type="email" value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Your Email" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                        placeholder="Password" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary"  onClick={() => createUserWithEmailAndPassword(email, password)}>
                     Create Account
                 </Button>
                 <br /><br />
